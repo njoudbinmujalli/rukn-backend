b8e4888e7d77f17e0393e5e6aca0bd0e5a6e10f6
njoudbinmujalli <njouddfaah@gmail.com>
2026-07-15 23:07:58 +0300

    Remove API key

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 
 from google import genai
 
-GEMINI_API_KEY = "AQ.Ab8RN6LzYuT5Dvkx51wIZxERZbWc55vZG_cbor3o5YTfTnZ88w" 
+
+GEMINI_API_KEY = "YOUR_API_KEY_HERE"
 
 DB_PATH = "rukn.db"
 GEMINI_MODEL = "gemini-2.5-flash"
